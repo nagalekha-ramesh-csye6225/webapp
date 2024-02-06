7f7499f0b94486e3c40798afc5fdff3306234300
NagalekhaRamesh <ramesh.na@northeastern.edu>
2024-02-06 13:50:50 -0500

    Created User Model

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    account_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    account_updated: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    }
+}, {
+    tableName: 'users',
+    timestamps: false,
+    indexes: [
+        {
+            unique: true,
+            fields: ['username']
+        }
+    ]
+});
+
+module.exports = User;
