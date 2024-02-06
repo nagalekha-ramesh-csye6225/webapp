8f039bbd21da5298081dfd1732aed281322bcd03
NagalekhaRamesh <ramesh.na@northeastern.edu>
2024-02-06 10:21:09 -0500

    Maintaining DB credentials in env

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('assignment_01', 'cloud_user', '1234', {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
 });
 
 module.exports = { sequelize };
