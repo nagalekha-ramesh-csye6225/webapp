7f0709fe0f0eb0555e65088f5f17d14e1e09d101
NagalekhaRamesh <ramesh.na@northeastern.edu>
2024-02-07 23:12:32 -0500

    Making changes for Input Validations

--- middleware/validateUpdateUserData.js
+++ middleware/validateUpdateUserData.js
@@ -1,20 +1,20 @@
-const { validateUsername, validatePassword, validateName } = require('../validators/userValidators')
+const { validatePassword, validateName } = require('../validators/userValidators')
 
 const validateUpdateUserData = async (req, res, next) => {
-    // Check if the request body contains the fields to be updated
-    const { first_name, last_name, password } = req.body;
-    if (first_name === undefined && last_name === undefined && password === undefined) {
-        return res.status(400).json({ message: 'At least one field to update must be provided' });
-    }
-
     // Validate if any other field is attempted to be updated
     const invalidFields = Object.keys(req.body).filter(field => !['first_name', 'last_name', 'password'].includes(field));
     if (invalidFields.length > 0) {
         return res.status(400).json({ message: 'Invalid field(s) for update: ' + invalidFields.join(', ') });
     }
 
+    // Check if the request body contains the fields to be updated
+    const { first_name, last_name, password } = req.body;
+    if (first_name === undefined && last_name === undefined && password === undefined) {
+        return res.status(400).json({ error: 'At least one field to update must be provided' });
+    }
+
     if(first_name === ""){
-        return res.status(400).json({message: 'First name cannot be null'});
+        return res.status(400).json({error: 'First name cannot be null'});
     }
 
     if(last_name === ""){
@@ -25,15 +25,27 @@ const validateUpdateUserData = async (req, res, next) => {
         return res.status(400).send({error: 'Password cannot be null'})
     }
 
-    if(!validateName(first_name)){
+    if(password!== undefined  && password.length < 8){
+        return res.status(400).send({error: 'Password should be atleast 8 characters long'})
+    }
+
+    if(first_name!==undefined && first_name.length < 1){
+        return res.status(400).send({error: 'First Name should be of minimum length : 1'})
+    }
+
+    if(last_name!==undefined && last_name.length < 1){
+        return res.status(400).send({error: 'Last Name should be of minimum length : 1'})
+    }
+
+    if(first_name!== undefined && !validateName(first_name)){
         return res.status(400).send({error: 'First name not in right format'});
     }
 
-    if(!validateName(last_name)){
+    if(last_name!== undefined && !validateName(last_name)){
         return res.status(400).send({error: 'Last name not in right format'});
     }
 
-    if(!validatePassword(password)){
+    if(password!== undefined && !validatePassword(password)){
         return res.status(400).send({error: 'Password not in right format'});
     }
 
