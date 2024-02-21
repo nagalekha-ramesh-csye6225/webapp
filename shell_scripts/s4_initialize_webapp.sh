fccd5fb5be33f10da81dd96a8358a68b789876f4
NagalekhaRamesh <ramesh.na@northeastern.edu>
2024-02-21 13:46:58 -0500

    Moving setup postgres db & user block from script 2 to  script 4

--- shell_scripts/s4_initialize_webapp.sh
+++ shell_scripts/s4_initialize_webapp.sh
@@ -9,5 +9,15 @@ cd /opt/csye6225/webapp/
 # Logging env file
 cat .env
 
+# Load environment variables
+source .env || { echo "Failed to load environment variables from .env file."; exit 1; }
+
+# Set up db & user
+sudo -u postgres psql -c "CREATE DATABASE $DATABASE_NAME;"
+sudo -u postgres psql -c "CREATE USER $DATABASE_USERNAME WITH PASSWORD '$DATABASE_PASSWORD';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DATABASE_NAME TO $DATABASE_USERNAME;"
+sudo sed -i.bak 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf || { echo "Failed to update pg_hba.conf."; exit 1; }
+sudo systemctl restart postgresql || { echo "Failed to restart PostgreSQL service."; exit 1; }
+
 # Install node packages
 sudo npm install
